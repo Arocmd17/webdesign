aa579a3c161759fbfc3d4d21f2f924eb86439909
Arocmd9 <akanderuben17@gmail.com>
2020-08-12 17:30:13 +0100

    first commit

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/dist/'));
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'build/index.html'));
+    res.sendFile(path.join('dist/index.html'));
 })
 
 app.listen( PORT,()=>{
